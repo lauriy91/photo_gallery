b0b5495643ca71d441a475f162a7dc81ec5e72de
lauriy91 <lauriys91@gmail.com>
2022-01-05 22:02:19 -0500

    iuser

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-import express, { Request, Response} from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 // Para manejar sesiones y guardar la info del usuario cuando esté autenticado
-import session, {Session} from "express-session";
+import session, { Session } from "express-session";
 import { join } from "path";
 import mongoose from "mongoose";
 
 // importación de las rutas
 import {router as homeRouter} from "./routes/login.route";
+
+declare module "express-session" {
+    interface Session {
+      user: IUser;
+    }
+}
 
 export const app = express();
 
@@ -16,6 +22,7 @@ dotenv.config();
 
 // para hacer llamados a recursos publicos
 app.use(express.static(join(__dirname, "../", "public")));
+app.use(express.urlencoded({ extended: false }));
 // motor de template
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -27,14 +34,11 @@ app.use(cors());
 // configuración de sesión
 app.use(
     session({
-        secret: "this is the secret",
-        resave: false,
-        saveUninitialized: false,
+      secret: "ssshhhhh",
+      saveUninitialized: true,
+      resave: true,
     })
 );
-
-// Indicación para direccionar a home
-app.use(homeRouter);
 
 // Conexión
 const options: mongoose.ConnectOptions = {
@@ -46,15 +50,22 @@ const options: mongoose.ConnectOptions = {
 // conexion a la base de datos
 (async () => {
     await mongoose.connect(process.env.DB_CONNECTION as string, options);
-    console.log("Conectado a Mongo DB ... ");
+    console.log("Conectado a Mongo DB...");
 })();
 
+// Indicación para direccionar a home
+app.use(homeRouter);
+
 // Preparamos entorno "/" para la ruta, req los requerimiento y resp las respuestas
-app.get("/", (req: Request, res: Response) =>{
-    res.send("Hola mundo");
+app.get("/", (req: Request, res: Response) => {
+    res.render("index");
+});
+
+app.use(function (req, res, next) {
+    res.render("error/404");
 });
 
 // Puerto de escucha
-app.listen(3000, () =>{
-    console.log("Servidor en el puerto 3000");
+app.listen(3000, () => {
+    console.log("Connection successful...");
 });
